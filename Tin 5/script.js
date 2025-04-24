b7b6e8cee9c32c3f15bb78f164f6d26727bf2cc5
pHong1807 <phongtan1098@gmail.com>
2025-04-24 21:13:26 +0700

    Delete Tin 5 directory

--- Tin 5/script.js
+++
@@ -1,6 +0,0 @@
-const list = document.getElementById('newsList');
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-    
-prev.onclick = () => list.scrollBy({ left: -300, behavior: 'smooth' });
-next.onclick = () => list.scrollBy({ left: 300, behavior: 'smooth' });
